e6df0d22a1ecc1a18bf6f7fb30795d98aff7a6ce
Seoptrike <dlstjq977@naver.com>
2025-08-11 20:52:20 +0900

    fix: cors cofig

--- src/main/java/com/backend/CorsConfig.java
+++ src/main/java/com/backend/CorsConfig.java
@@ -1,6 +1,7 @@
 package com.backend;
 
 import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,29 +10,29 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CorsConfig {
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration config = new CorsConfiguration();
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
 
-    // 허용할 출처(Origin 설정)
-    config.setAllowedOrigins(
-        Arrays.asList("http://localhost:5173", "https://seoportfolio.vercel.app"));
+        // 허용할 출처(Origin 설정)
+        config.setAllowedOrigins(
+                Arrays.asList("http://localhost:5173", "https://portfolio-front-dun.vercel.app"));
 
-    // 허용할 HTTP 메서드 설정
-    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        // 허용할 HTTP 메서드 설정
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
 
-    // 허용할 요청 헤더 설정
-    config.setAllowedHeaders(Arrays.asList("*"));
+        // 허용할 요청 헤더 설정
+        config.setAllowedHeaders(Arrays.asList("*"));
 
-    // 브라우저에 노출할 헤더 설정 (예: JWT 토큰)
-    config.setExposedHeaders(Arrays.asList("Authorization", "Authorization-refresh"));
+        // 브라우저에 노출할 헤더 설정 (예: JWT 토큰)
+        config.setExposedHeaders(Arrays.asList("Authorization", "Authorization-refresh"));
 
-    // 자격 증명(쿠키, 인증 헤더 등) 허용 여부
-    config.setAllowCredentials(true);
+        // 자격 증명(쿠키, 인증 헤더 등) 허용 여부
+        config.setAllowCredentials(true);
 
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", config); // 모든 경로에 대해 위 설정 적용
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // 모든 경로에 대해 위 설정 적용
 
-    return source;
-  }
+        return source;
+    }
 }
