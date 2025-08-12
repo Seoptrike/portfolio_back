85715cfec33ee6972e4d133578fa39d54d7329a5
Seoptrike <dlstjq977@naver.com>
2025-08-12 10:21:46 +0900

    fix: add @EnableJdbcHttpSession

--- src/main/java/com/backend/BackendApplication.java
+++ src/main/java/com/backend/BackendApplication.java
@@ -3,8 +3,10 @@ package com.backend;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication(scanBasePackages = "com.backend")
+@EnableJdbcHttpSession
 @MapperScan("com.backend.mapper")
 public class BackendApplication {
 
